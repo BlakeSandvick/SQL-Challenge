fbde4abaecb75a5a1822619130dfdd76e4d73efb
BlakeSandvick <blake.sandvick@gmail.com>
2024-03-09 14:26:04 -0600

    Completed work

---
+++ Tables-Filtering_Code.sql
@@ -0,0 +1,83 @@
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date VARCHAR NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE departments(
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
+
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%';
+
+SELECT * FROM departments;
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd007';
+
+SELECT * FROM departments;
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd005' or departments.dept_no = 'd007';
+
+SELECT last_name, COUNT(last_name) AS "Frequency count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency count" DESC;
